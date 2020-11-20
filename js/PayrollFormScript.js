e4a229ad97f3ea58789007aa326699aa6c0db38d
Jay002 <jayesh2598@gmail.com>
2020-11-20 12:18:39 +0530

    Added: Created employeePayrollData object on submit and displayed it in alert box.

--- js/PayrollFormScript.js
+++ js/PayrollFormScript.js
@@ -54,3 +54,50 @@ window.addEventListener('DOMContentLoaded', (event) => {
       output.textContent = salary.value;
    });
 });
+
+const save = () => {
+   try {
+      const employeePayrollData = createEmployeePayroll();
+   } catch (e) {
+      return;
+   }
+}
+
+const createEmployeePayroll = () => {
+   let employeePayrollData = new EmployeePayrollData();
+   try {
+      employeePayrollData.name = getInputValueById('#name');
+   } catch (e) {
+      setTextValue('.text-error', e);
+      throw e;
+   }
+   employeePayrollData.profilePic = getSelectedValues('[name=profile]').pop();
+   employeePayrollData.gender = getSelectedValues('[name=gender]').pop();
+   employeePayrollData.department = getSelectedValues('[name=department');
+   employeePayrollData.salary = getInputValueById('#salary');
+   employeePayrollData.notes = getInputValueById('#notes');
+   let date = getInputValueById('#day') + " " + getInputValueById('#month') + " " + getInputValueById('#year');
+   employeePayrollData.startDate = new Date(getInputValueById('#year'), getInputValueById('#month')-1, getInputValueById('#day'));
+   alert(employeePayrollData.toString());
+   return employeePayrollData;
+}
+
+const getSelectedValues = (propertyValue) => {
+   let allItems = document.querySelectorAll(propertyValue);
+   let selectedItems = [];
+   allItems.forEach(item => {
+      if(item.checked)
+         selectedItems.push(item.value);
+   });
+   return selectedItems;
+}
+
+const getInputValueById = (id) => {
+   let value = document.querySelector(id).value;
+   return value;
+}
+
+const getInputElementValue = (id) => {
+   let value = document.getElementById(id).value;
+   return value;
+}
